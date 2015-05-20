09ee557b52d990a16785485bcdeb5b7a2c1dee37
Juzet Arana <arana.juzet@gmail.com>
2015-05-20 07:49:21 -0700

    Beginning of the project (index almost done)

---
+++ projects.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style2.css">
+</head>
+<nav>
+	<ul>
+		<a href="http://localhost/appacademy/index.php">Instagram</a>
+	</ul>
+<body>
+</body>
+</html>
