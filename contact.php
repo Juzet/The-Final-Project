2bcd48db64fd352d5506828d28258b29db11c657
Juzet Arana <arana.juzet@gmail.com>
2015-05-21 15:14:41 -0700

    changing project completely

---
+++ contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style3.css">
+</head>
+<nav>
+</nav>
+<body>
+<address>
+	Designed By: <a href="ara	na.juzet@gmail.com">Juzet Arana</a><br> 
+</address>
+</body>
+</html>
--- index.php
+++ index.php
@@ -8,22 +8,16 @@
 <div class="nav">
 	<ul>
 	<div id="a">
-		<a href="http://localhost/landingpage/index.php">HOME</a>
-		<a href="http://localhost/landingpage/projects.php">PROJECTS</a>
+	<a href="http://localhost/landingpage/projects.php">PROJECTS</a>
     <a href="http://localhost/landingpage/about.php">ABOUT ME</a>		  
    	<a href="http://localhost/landingpage/contact.php">CONTACT</a>
    	</div>
-	<form action="http://localhost/landingpage/" method="post">
-			<input type="text" name="q" id="search-input" placeholder="Search Juzet Arana's Landing Webpage..." />
-	<div id="one"><label for="search-input"><label></div>
-  </form>
-	</ul>
-	</div>
-	</ul>
+   	</ul>
 </div>
 <body>
 	<div id="next">
-		Juzet Arana's Landing Webpage
+		Juzet Arana
 	</div>
+
 </body>
 </html>
