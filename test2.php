8507785a568860a89ff5f535b310358eca7391f0
Juzet Arana <arana.juzet@gmail.com>
2015-05-26 11:28:32 -0700

    new project again

---
+++ test2.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/two.css">
+</head>
+<body>
+	<div class="parallax">
+	<div id="group1" class="parallaxGroup">
+    <div class="parallaxLayer parallaxLayerBase">
+      <div class="center">
+        JUZET ARANA
+        	<p class="arana">
+			"Great web design without functionality is like a sports car with no engine."
+			<br>
+			~Paul Cookson~
+			</br>
+		</p>
+      </div>
+    </div>
+  </div>
+  <div id="group2" class="parallaxGroup">
+    <div class="parallaxLayer parallaxLayerBack">
+      <div class="center">
+      </div>
+    </div>
+  </div>
+  <div id="group3" class="parallaxGroup">
+    <div class="parallaxLayer parallaxLayerBase">
+  		<p class="slogan"><span>Like it? Message me!</span></p>
+    </div>
+  </div>
+</div>
+</body>
+</html>
