effaf6bb2e19f63724c811fb237498747550b034
Juzet Arana <arana.juzet@gmail.com>
2015-05-27 11:21:05 -0700

    index okay

--- test2.php
+++ test2.php
@@ -1,32 +1,32 @@
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="css/two.css">
+ <link rel="stylesheet" type="text/css" href="css/two.css">
 </head>
 <body>
-	<div class="parallax">
-	<div id="group1" class="parallaxGroup">
+ <div class="parallax">
+ <div id="group1" class="parallaxGroup">
     <div class="parallaxLayer parallaxLayerBase">
-      <div class="center">
+       <div class="center">
         JUZET ARANA
-        	<p class="arana">
-			"Great web design without functionality is like a sports car with no engine."
-			<br>
-			~Paul Cookson~
-			</br>
-		</p>
+         <p class="arana">
+     "Great web design without functionality is like a sports car with no engine."
+    <br>
+     ~Paul Cookson~
+     </br>
+  </p>
       </div>
-    </div>
-  </div>
-  <div id="group2" class="parallaxGroup">
+   </div>
+ </div>
+ <div id="group2" class="parallaxGroup">
     <div class="parallaxLayer parallaxLayerBack">
       <div class="center">
       </div>
-    </div>
+   </div>
   </div>
-  <div id="group3" class="parallaxGroup">
-    <div class="parallaxLayer parallaxLayerBase">
-  		<p class="slogan"><span>Like it? Message me!</span></p>
-    </div>
+ <div id="group3" class="parallaxGroup">
+   <div class="parallaxLayer parallaxLayerBase">
+    <p class="slogan"><span>Like it? Message me!</span></p>
+   </div>
   </div>
 </div>
 </body>
